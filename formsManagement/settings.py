8160653d4521ff7aac5fd31d886f5f761120c1aa
muradddd <murad.rustemzade@gmail.com>
2020-08-11 00:18:50 +0400

    core & accounts app added

--- formsManagement/settings.py
+++ formsManagement/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local apps
+    'core.apps.CoreConfig',
+    'accounts.apps.AccountsConfig',
+
+    # Third-party modules
 ]
 
 MIDDLEWARE = [
