179c6d1753af2cc90fa7e9631c8ffdecaca30489
muradddd <murad.rustemzade@gmail.com>
2020-08-14 20:13:39 +0400

    admin inline

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,35 @@
 from django.contrib import admin
 from core.models import *
 
-# Register your models here.
-admin.site.register([Field,Forms])
+
+
+
+class OptionInline(admin.StackedInline):
+    model = Option
+
+
+class FieldInline(admin.StackedInline):
+    model = Field
+
+
+@admin.register(Field)
+class FieldAdmin(admin.ModelAdmin):
+    fieds = ('__all__')
+
+
+@admin.register(Option)
+class OptionAdmin(admin.ModelAdmin):
+    fieds = ('__all__')
+
+class ResponsenInline(admin.TabularInline):
+    model = ResponseQA
+
+@admin.register(Response)
+class ResponseAdmin(admin.ModelAdmin):
+    fieds = ('__all__')
+    inlines = [ResponsenInline,]
+
+
+@admin.register(Forms)
+class FormAdmin(admin.ModelAdmin):
+    inlines = [FieldInline,]
